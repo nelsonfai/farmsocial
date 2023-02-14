156dd9082559a3e22668a9c3fd6dfd948ef950e8
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-02-14 19:09:03 +0100

    Update asgi.py

--- myagricdiary/asgi.py
+++ myagricdiary/asgi.py
@@ -4,7 +4,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
+print('from my asgi file')
 import os
 import chat.routing
 from django.core.asgi import get_asgi_application
