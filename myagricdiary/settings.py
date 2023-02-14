122ef8b3c5477e71f0e398a460cf963d1f77f939
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-02-14 12:18:56 +0100

    db update

--- myagricdiary/settings.py
+++ myagricdiary/settings.py
@@ -98,13 +98,13 @@ TEMPLATES = [
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 
