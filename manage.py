db33b3b4bcd62eed448f82611b346d977a234a7d
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-02-14 11:51:50 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,8 +8,8 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-     os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
-     try:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
