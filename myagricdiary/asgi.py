1155351b042a45c3be4d7b9c7bdffa82c2355913
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-02-14 19:15:45 +0100

    Update asgi.py

--- myagricdiary/asgi.py
+++ myagricdiary/asgi.py
@@ -11,7 +11,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myagricdiary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smyagricdiary.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
