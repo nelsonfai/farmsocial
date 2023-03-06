e218810e91cba94f7f048902ce125672204f7a0a
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-06 23:36:59 +0100

    message notifiaciton

--- chat/context_processors.py
+++ chat/context_processors.py
@@ -1,17 +1,15 @@
 from .models import Thread,ChatMessage
-
+from django.db.models import Q
 def get_unread_messages_count(request):
     if  request.user.is_authenticated:
         user= request.user
         # Get all threads involving the user
-        threads = Thread.objects.by_user(user=user)
-        
+        threads = Thread.objects.filter(Q(first_person=user) | Q(second_person=user))
         # Count the number of unread messages in all threads involving the user
         count = ChatMessage.objects.filter(
             thread__in=threads,  # Filter by threads involving the user
-            user__ne=user,  # Filter out messages sent by the user
             is_read=False,  # Filter by unread messages
-        ).count()
+        ).exclude(user=user).count()
         
         return {'unread':count}
     else:
