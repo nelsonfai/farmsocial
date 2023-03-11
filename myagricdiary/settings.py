e6fff9e05c2525fc2918da4a83ca7e41faae83ff
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-11 22:41:01 +0100

    Update settings.py

--- myagricdiary/settings.py
+++ myagricdiary/settings.py
@@ -187,7 +187,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    #'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 
