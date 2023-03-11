93ce68df38f09af9c0642afe98238781b45c3445
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-11 15:53:09 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,12 +25,14 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    """ 
+
+]
+
+""" 
     path('google/login/', LoginView.as_view(
         template_name='accounts/google_login.html', 
         #authentication_method='google',
         extra_context={'title': 'Google Login'}
     ), name='account_signup'),
     
-  """
-]
+"""
