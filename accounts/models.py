9f5e2360763adf8257f952e4e4182b9f0355cc95
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-14 00:02:31 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -19,12 +19,13 @@ def validate_file_size(value):
         )
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password,phonenumber, **extra_fields):
 
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        if  email:
+            email = self.normalize_email(email)
+        if phonenumber:
+            phonenumber = self
+        user = self.model(email=email,phonenumber=phonenumber, **extra_fields)
         user.set_password(password)
         
         user.save(using=self._db)
