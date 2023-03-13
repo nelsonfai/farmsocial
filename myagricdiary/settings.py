a09e7ac1903fdd956f0db307ba9aff823a92ecc4
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-13 18:40:26 +0100

    Update settings.py

--- myagricdiary/settings.py
+++ myagricdiary/settings.py
@@ -60,13 +60,7 @@ INSTALLED_APPS = [
     'myagricai',
     'company',
       "phonenumber_field",
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-
-    
+ 
 ]
 
 MIDDLEWARE = [
