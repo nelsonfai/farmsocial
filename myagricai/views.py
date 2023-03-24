2058f9fdf442c153a315d11582572b00a5dd2b0a
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-24 12:06:46 +0100

    agric ai

--- myagricai/views.py
+++ myagricai/views.py
@@ -33,7 +33,7 @@ def openai(question):
     model="text-davinci-003",
     temperature=0,
     prompt=question,
-    max_tokens=100,
+    max_tokens=150,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.0,
