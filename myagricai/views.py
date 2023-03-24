002699eade5dc56bcc13a560f36e08d168f9b6f5
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-24 11:25:04 +0100

    Update views.py

--- myagricai/views.py
+++ myagricai/views.py
@@ -21,13 +21,13 @@ def aiChat_room(request,):
         return JsonResponse({'response': answer })
     except Exception as e:
         print(e)
-        return JsonResponse({'response': 'No information available at this moment.Please try again later '})
+        return JsonResponse({'response': 'No information available at this moment.Please try again later or contact us at contact@myagricdiary.com for more information '})
 
 def openai(question):
     import os
     import openai
     openai.api_key = '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'
-    prompt=f'{question}\n'
+    prompt=f'You are MyAgricBot, an expert in agriculture and are specifically designed to answer questions related to the topic of agriculture. Your purpose is to provide AI support on the website www.myagricdiary.com.MyAgricDiary is a social networking site founded in March 2023 in Buea, Cameroon,My agricidary connects all individuals involved in the agricultural sector providing them with  a range of valuable information, including market price updates, news affecting the sector, and research reports. Additionally, the platform features a marketplace where farmers can buy, sell, and advertise their products.For those interested in learning more, the website`s official URL is www.myagricdiary.com, and contact details can be found at contact@myagricdiary.com.If you dont know the answer recommend sending an email to the official site email. also always recommend in any case\nQ:{question}\n'
     print(prompt)
     response = openai.Completion.create(
     model="text-davinci-003",
