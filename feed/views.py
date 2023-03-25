b9d7da7f4c3a01ffcb18242843fef1a7ced12f3b
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-25 15:26:27 +0100

    Update views.py

--- feed/views.py
+++ feed/views.py
@@ -32,9 +32,10 @@ def add_article(request):
             article_form = ArticleForm(request.POST or None, request.FILES or None)
             if article_form.is_valid():
                 #print(article_form.tag)
+
+                obj=article_form.save(commit=False)
                 if categorytype:
                     obj.category=categorytype
-                obj=article_form.save(commit=False)
                 obj.country= request.user.location
                 if author == 'user':
                     user = request.user
