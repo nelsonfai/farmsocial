2ec12eb760ffe96912592e26a49d681543493fba
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-03-25 13:53:27 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -253,6 +253,9 @@ class SignupWizard(SessionWizardView):
 
         token = str(uuid.uuid4())
         unique_id=str(uuid.uuid4().hex)[:8]
+        first_name=form_list[2].cleaned_data['first_name'],
+        last_name=form_list[2].cleaned_data['last_name'],
+        
         if form_list[0].cleaned_data['email'] or form_list[0].cleaned_data['phonenumber']:
             pass
         else:
