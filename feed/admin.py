01557bc47747eafaec9c79cf7897b349e2a81644
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-04-03 16:06:46 +0200

    template images

--- feed/admin.py
+++ feed/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Articles,Comments,Like,Announcements
+from .models import Articles,Comments,Like,Announcements,Images
 # Register your models here.
 admin.site.register(Articles)
 admin.site.register(Comments)
 admin.site.register(Like)
+admin.site.register(Images)
 admin.site.register(Announcements)
