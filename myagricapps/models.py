74dafbe9e7627a2421e6a0572509aa7591576ecb
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-04-05 00:51:48 +0200

    myagricdapps

--- myagricapps/models.py
+++ myagricapps/models.py
@@ -1,17 +1,3 @@
-from django.db import models
-from django.db.models import  Manager
 
-
-# Create your models here.
-from accounts.models import CustomUser
-
-class UserQA(models.Model):
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    question=models.CharField(max_length=1000)
-    answer = models.CharField(max_length=1000)
-    date= models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.question
-    objects = Manager()   # The default Manager.
 
 
